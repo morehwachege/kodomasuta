10fb8de3977e42c05c7ad3a8fa8232ce006c0256
JasonKiptoo <kiptoojason@gmail.com>
2022-12-06 01:30:20 +0300

    added the login page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,66 +3,49 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <div class="container py-5 h-100">
+      <div class="container-login">
+        <h1 class="fw-bold mb-2 py-3 p-3">Kodomasuta</h1>
+        <div className="container py-1 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
           <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card bg-light text-dark">
+            <div class="login-card">
               <div class="card-body p-5 text-center">
                 <div class="mb-md-5 mt-md-4 pb-5">
-                  <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                  <p class="text-dark-50 mb-5">
-                    Code Like Never Before
-                  </p>
+                  <h2 class="fw-bold mb-2 ">Login</h2>
+                  <p class="text-dark-50 mb-5">code like never before...</p>
                   <div class="form-outline form-white mb-4">
                     <input
                       type="email"
                       id="typeEmailX"
-                      class="form-control form-control-lg"
+                      class="form-control form-control-lg" placeholder="Email"
                     />
-                    <label class="form-label" for="typeEmailX">
-                      Email
-                    </label>
+
                   </div>
                   <div class="form-outline form-white mb-4">
                     <input
                       type="password"
                       id="typePasswordX"
-                      class="form-control form-control-lg"
+                      class="form-control form-control-lg" placeholder="Password"
                     />
-                    <label class="form-label" for="typePasswordX">
-                      Password
-                    </label>
+
                   </div>
                   <p class="small mb-5 pb-lg-2">
-                    <a class="text-white-50" href="#!">
+                    <a class="text-dark-50" href="#!">
                       Forgot password?
                     </a>
                   </p>
 
-                  <button
-                    class="btn btn-primary btn-lg px-5"
-                    type="submit"
-                  >
+                  <button class="btn btn-primary btn-lg px-5" type="submit">
                     Login
                   </button>
 
-                  <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                    <a href="#!" class="text-white">
-                      <i class="fab fa-facebook-f fa-lg"></i>
-                    </a>
-                    <a href="#!" class="text-white">
-                      <i class="fab fa-twitter fa-lg mx-4 px-2"></i>
-                    </a>
-                    <a href="#!" class="text-white">
-                      <i class="fab fa-google fa-lg"></i>
-                    </a>
-                  </div>
+                 
                 </div>
 
                 <div>
                   <p class="mb-0">
                     Don't have an account?{" "}
-                    <a href="#!" class="text-white-50 fw-bold">
+                    <a href="#!" class="text-dark-50 fw-bold">
                       Sign Up
                     </a>
                   </p>
@@ -71,6 +54,7 @@ const Login = () => {
             </div>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
