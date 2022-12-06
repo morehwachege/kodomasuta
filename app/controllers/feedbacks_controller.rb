7015c619b458e759f758c0683b20a02820156216
Felix <barosiofelix@gmail.com>
2022-12-06 22:48:44 +0300

    add: index, show

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,28 @@
 class FeedbacksController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    # GET /feedbacks
+    def index
+      @feedback = Feedback.all
+      render json: @feedback
+    end
+
+    # GET /feedbacks/:id
+    def show
+      @feedback = find_feedback
+      render json: @feedback, status: :ok
+    end
+
+    private
+
+    def find_feedback
+        Feedback.find(params[:id])
+    end
+    
+    # feedback not found response
+    def render_not_found_response
+        render json: { errors: "Feedback not found"}, status: :not_found
+    end
+
 end
