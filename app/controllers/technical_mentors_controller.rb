9d9df39dd57fe87b4d6b854a7c7e6625cd39f83e
Anne Ogongo <anneogongo98@gmail.com>
2022-12-06 18:41:18 +0300

    routes done

--- app/controllers/technical_mentors_controller.rb
+++ app/controllers/technical_mentors_controller.rb
@@ -1,2 +1,48 @@
 class TechnicalMentorsController < ApplicationController
+
+    def index
+        technical_mentors = TechnicalMentor.all
+        render json: technical_mentors
+    end
+
+    def show
+        technical_mentor = TechnicalMentor.find_by(id: params[:id])
+        if technical_mentor
+          render json: technical_mentor
+        else
+          render json: { error: "Technical Mentor not found" }, status: :not_found
+        end
+    end
+
+    def update
+        technical_mentor = TechnicalMentor.find_by(id: params[:id])
+        if technical_mentor
+          technical_mentor.update(technical_mentor_params)
+          render json: technical_mentor
+        else
+          render json: { error: "Technical Mentor not found" }, status: :not_found
+        end
+      end
+
+    def create
+      technical_mentor = TechnicalMentor.create(technical_mentor_params)
+      render json: technical_mentor, status: :created
+    end
+
+    def destroy
+        technical_mentor = TechnicalMentor.find_by(id: params[:id])
+        if technical_mentor
+          technical_mentor.destroy
+          head :no_content
+        else
+          render json: { error: "Technical-Mentor not found" }, status: :not_found
+        end
+      end
+
+      private
+
+      def technical_mentor_params
+        params.permit(:firstname, :lastname, :email)
+      end
+
 end
