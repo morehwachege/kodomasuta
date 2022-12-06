fa19a62949591e6182562db1ef05752d3c5080f1
Anne Ogongo <anneogongo98@gmail.com>
2022-12-06 13:47:25 +0300

    Removed extra column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_06_102448) do
+ActiveRecord::Schema.define(version: 2022_12_06_104647) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2022_12_06_102448) do
   create_table "technical_mentors", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "TechnicalMentor"
     t.string "firstname"
     t.string "lastname"
     t.string "email"
