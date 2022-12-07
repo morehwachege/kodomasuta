3476a38d6a1f72a34a3113a16d326f95e4a871f0
Felix <barosiofelix@gmail.com>
2022-12-07 14:41:43 +0300

    add: feedback card and icon

---
+++ client/src/components/Feedback/feedbackcard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FeedBackCard = () => {
+    return (
+        <div className='feedback_container'>
+            <div className="feedback_options">
+                <article class="feedback_option">
+                    <img className="feedback_icon" src={"https://i.pinimg.com/564x/fa/d8/3b/fad83b229d4c0666e3f7d45035af5404.jpg"} alt="" />
+                </article>
+            </div>
+        </div>
+    )
+}
+
+export default FeedBackCard;
