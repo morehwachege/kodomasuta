4c859f5c353e2a0e4aa8bce20ed02fa8872f7f32
Felix <barosiofelix@gmail.com>
2022-12-07 13:57:07 +0300

    add: route for feedback

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import Dashboard from "./components/dashboard";
+import Feedback from "./components/Feedback/feeback";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/feedback" element={<Feedback />} />
     </Routes>
 
   );
