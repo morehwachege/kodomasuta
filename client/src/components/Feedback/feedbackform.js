560c3aecbbc8cfee2d711bd7eb1a0ca1592fa24c
Felix <barosiofelix@gmail.com>
2022-12-07 14:28:28 +0300

    add: contsainer

---
+++ client/src/components/Feedback/feedbackform.js
@@ -0,0 +1,11 @@
+import React, { useRef } from 'react'
+
+const FeedBackForm = () => {
+
+    return (
+        <div className='feedback_container'>
+        </div>
+    )
+}
+
+export default FeedBackForm;
