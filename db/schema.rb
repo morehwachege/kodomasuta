ba000be5e4eb0fa99eda4431fa8da8b68ddd70cd
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-12-07 03:07:31 +0300

    add: seeds

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,25 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_06_113529) do
+ActiveRecord::Schema.define(version: 2022_12_06_233734) do
+
+  create_table "assessments", force: :cascade do |t|
+    t.string "title"
+    t.integer "category_id"
+    t.text "description"
+    t.integer "technical_mentor_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -11,6 +31,25 @@ ActiveRecord::Schema.define(version: 2022_12_06_113529) do
     t.string "title"
     t.text "content"
     t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.string "choice1"
+    t.string "choice2"
+    t.string "choice3"
+    t.string "correct_answer"
+    t.integer "assessment_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "student_assessments", force: :cascade do |t|
+    t.string "assessment_id"
+    t.string "student_id"
+    t.integer "grade"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
