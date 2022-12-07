7cee32c054486b43508f7965fe3f7970cca5fee6
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-12-07 14:46:56 +0300

    fix: logo

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -4,32 +4,32 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="logincontainer">
-      <h2 className="logo">Kodomasuta</h2>
+      <h2 className="logo">kodomasuta</h2>
       <div class="container py-1 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-12 col-md-8 col-lg-2 col-xl-5">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
             <div class="card-login">
               <div class="card-body p-5 text-center">
                 <div class="mb-md-5 mt-md-4 pb-5">
                   <h2 class="fw-bold mb-2 ">Login</h2>
-                  <p class="text-dark-50 mb-3">code like never before...</p>
+                  <p class="text-dark-50 mb-3 code">code like never before...</p>
 
                   <form>
-                    <div className="form-floating mb-3">
+                    <div className="form-floating mb-3 p-0">
                       <i class="icon fa-solid fa-envelope"></i>
                       <input
                         type="email"
-                        className="form-control bg-dark text-white"
+                        className="form-control bg-dark text-white pl-3"
                         id="floatingName"
                         placeholder="Email"
                       ></input>
                     </div>
                     <div className="form-floating mb-3">
-                      <i class="icon fa-solid fa-unlock-keyhole"></i>
-                      <i class="icon fa-light fa-unlock-keyhole"></i>
+                      {/* <i class="icon fa-solid fa-unlock-keyhole"></i>
+                      <i class="icon fa-light fa-unlock-keyhole"></i> */}
                       <input
                         type="password"
-                        className="form-control bg-dark text-white"
+                        className="form-control bg-dark text-white p-0"
                         id="floatingName"
                         placeholder="Password"
                       ></input>
@@ -53,7 +53,7 @@ const Login = () => {
                 </div>
 
                 <div>
-                  <p class="mb-0">
+                  <p class="mb-0 code">
                     Don't have an account?{" "}
                     <Link to="/signup">
                       <a href="" class="text fw-bold">
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const Signup = () => {
   return (
     <div className="logincontainer">
-      <h2 className="logo">Kodomasuta</h2>
+      <h2 className="logo">kodomasuta</h2>
       <div class="container py-1 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-12 col-md-8 col-lg-2 col-xl-5">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
             <div class="card-login">
               <div class="card-body p-5 text-center">
                 <div class="mb-md-5 mt-md-4 pb-5">
                   <h2 class="fw-bold mb-2 ">Signup</h2>
-                  <p class="text-dark-50 mb-5">code like never before...</p>
+                  <p class="text-dark-50 mb-5 code">code like never before...</p>
 
                   <form>
                     <div className="form-floating mb-3">
@@ -46,7 +46,7 @@ const Signup = () => {
                 </div>
 
                 <div>
-                  <p class="mb-0">
+                  <p class="mb-0 code">
                     Already have an account?{" "}
                     <Link to="/login">
                       <a href="#!" class="text fw-bold">
