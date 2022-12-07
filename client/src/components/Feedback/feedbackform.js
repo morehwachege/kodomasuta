f489ea063e96f275719d534e18dded4db67ce99f
Felix <barosiofelix@gmail.com>
2022-12-07 14:31:36 +0300

    add: fetch from api

--- client/src/components/Feedback/feedbackform.js
+++ client/src/components/Feedback/feedbackform.js
@@ -5,8 +5,26 @@ const FeedBackForm = ({ onAddItem }) => {
 
     const form = useRef()
 
+    const [title, setTItle] = useState("");
+    const [content, setContent] = useState("");
+
     function handleSubmit(e) {
         e.preventDefault();
+        const newFeedback = {
+            title: title,
+            content: content,
+        };
+        fetch("/feedbacks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newFeedback),
+        })
+            .then((res) => res.json())
+            .then((newItem) => onAddItem(newItem));
+        setTItle("");
+        setContent("");
     }
 
     return (
