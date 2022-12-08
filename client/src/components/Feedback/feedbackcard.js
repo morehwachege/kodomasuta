fa0281fb9a760ce7a29a2754a948419132063302
Felix <barosiofelix@gmail.com>
2022-12-08 14:04:56 +0300

    add: delete function

--- client/src/components/Feedback/feedbackcard.js
+++ client/src/components/Feedback/feedbackcard.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
-const FeedBackCard = ({ feedback: { title, content } }) => {
+const FeedBackCard = ({ feedback: { question_id, title, content }, feedback, onItemDelete }) => {
+
+    // Handle delete feedback
+    function handleDelete() {
+        fetch(`/feedbacks/${feedback.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json)
+            .then(() => onItemDelete(feedback));
+    }
+
     return (
         <div className='feedback_container'>
             <div className="feedback_options">
@@ -8,12 +21,18 @@ const FeedBackCard = ({ feedback: { title, content } }) => {
                     <img className="feedback_icon" src={"https://i.pinimg.com/564x/fa/d8/3b/fad83b229d4c0666e3f7d45035af5404.jpg"} alt="avatar" />
                     <div>
                         <h2 className='feed'>
-                            {title}
+                            <p className='feed_head'>Question Number:</p> {question_id}
                         </h2>
                         <h2 className='feed'>
-                            {content}
+                            <p className='feed_head'>Title:</p> {title}
+                        </h2>
+                        <h2 className='feed'>
+                            <p className='feed_head'>Description:</p> {content}
                         </h2>
                     </div>
+                    <button className="feedback_btn" onClick={handleDelete}>
+                        Delete
+                    </button>
                 </article>
             </div>
         </div>
