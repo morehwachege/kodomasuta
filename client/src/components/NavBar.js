e59ea7da7e2b7581bb3a6d396c83e7b8d9ba7473
JasonKiptoo <kiptoojason@gmail.com>
2022-12-08 10:08:59 +0300

    added the navbar to the dashboard

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function NavBar() {
+
+const Navbar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
-
-export default NavBar
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container px-lg-2">
+        <Link class="navbar-brand" to="/">
+          Kodomasuta
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <Link
+                class="nav-link active"
+                aria-current="page"
+                to="/assesments"
+              >
+                assesments
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/notifications">
+                notifications
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/logout">
+                Signout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
