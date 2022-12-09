41afd584e5e2b051b011d563f6356126befa709f
Felix <barosiofelix@gmail.com>
2022-12-09 11:16:27 +0300

    add: navbar and footer

--- client/src/components/Feedback/feedback.js
+++ client/src/components/Feedback/feedback.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import './feedback.css';
 import FeedBackForm from "./feedbackform";
 import FeedBackCard from "./feedbackcard"
+import Navbar from "../NavBar";
+import Footer from "../Footer";
 
 const Feedback = () => {
 
@@ -20,11 +22,13 @@ const Feedback = () => {
 
     return (
         <div>
+            <Navbar />
             <h2 className="feed_form">Assessment Feedback</h2>
             {feedbacks.map((feedback) => (
                 <FeedBackCard key={feedback.id} feedback={feedback} />))
             }
             <FeedBackForm handleNewFeedback={handleNewFeedback} feedbacks={feedbacks} />
+            <Footer />
         </div>
     );
 }
