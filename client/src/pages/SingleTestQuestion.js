093b55bd7669139a8994f6e69e90e7e629eb2aa4
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-12-10 03:26:47 +0300

    fix: test question card

--- client/src/pages/SingleTestQuestion.js
+++ client/src/pages/SingleTestQuestion.js
@@ -9,17 +9,17 @@ function SingleTestQuestion() {
                     Lara had been back and forth along the river path many times in her short life?
                 </p>
             </div>
-            <div className='container test-choices m-4'>
-                <div className='test-single-choice my-5 rounded d-flex align-items-center px-3'>
+            <div className='container test-choices m-4 d-flex justify-content-center flex-column align-items-center'>
+                <div className='test-single-choice mt-4 rounded d-flex align-items-center px-3'>
                     <p className='m-0'>Some choice</p>
                 </div>
-                <div className='test-single-choice my-5 rounded d-flex align-items-center px-3'>
+                <div className='test-single-choice mt-4 rounded d-flex align-items-center px-3'>
                     <p className='m-0'>Some choice</p>
                 </div>
-                <div className='test-single-choice my-5 rounded d-flex align-items-center px-3'>
+                <div className='test-single-choice mt-4 rounded d-flex align-items-center px-3'>
                     <p className='m-0'>Some choice</p>
                 </div>
-                <div className='test-single-choice my-5 rounded d-flex align-items-center px-3'>
+                <div className='test-single-choice mt-4 mb-4 rounded d-flex align-items-center px-3'>
                     <p className='m-0'>Some choice</p>
                 </div>
             </div>
