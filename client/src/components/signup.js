5cf7f59aa15103c76e85b839831e6e337c7d2116
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-12-30 12:53:06 +0300

    fix: signup proportions

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -24,80 +24,94 @@ const Signup = () => {
         password,
       }),
     })
-    .then((r) => {
-      // setIsLoading(false);
-      if (r.ok) {
-        r.json().then(() => navigate("/login"), console.log("success"));
-      } else {
-        r.json().then((err) => setErrors(err.errors));
-      }
-    });
+      .then((r) => {
+        // setIsLoading(false);
+        if (r.ok) {
+          r.json().then(() => navigate("/login"), console.log("success"));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
+        }
+      });
   }
   return (
-    <div className="logincontainer">
-      <h2 className="logo">kodomasuta</h2>
-      <div class="container py-1 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card-login">
-              <div class="card-body p-5 text-center">
-                <div class="mb-md-5 mt-md-4 pb-5">
-                  <h2 class="fw-bold mb-2 ">Signup</h2>
-                  <p class="text-dark-50 mb-5 code">
-                    code like never before...
-                  </p>
+    <>
+      <div className="container-fluid main-login">
+        <h2 className="logo">kodomasuta</h2>
+        <div className="login-container d-flex flex-column align-items-center justify-content-center">
+          <div className="container-fluid login-main p-0 d-flex justify-content-center align-items-center">
+            <div className="row w-100 d-flex justify-content-center align-items-center">
+              <div className="col-12 col-md-12 col-lg-6 col-xl-5">
+                <div class="card-login">
+                  <div class="card-body text-center py-5 px-5 mt-3">
+                    <div class="mb-md-5 mt-md-4 pb-3">
+                      <h2 class="fw-bold mb-2 ">Sign Up</h2>
+                      <p class="text-dark-50 mb-3 code">
+                        code like never before...
+                      </p>
+                      <form>
+                        <div className="form-floating mb-3">
+                          {/* <i class="icon fa-solid fa-envelope"></i> */}
+                          <input
+                            type="email"
+                            name="name"
+                            className="form-control bg-dark text-white py-0 px-3"
+                            id="floatingName"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                          <h4 className="errorhead text-danger">
+                            {" "}
+                            {errors.map((error) => error)}{" "}
+                          </h4>
+                        </div>
+                        <div className="form-floating mb-3">
+                          <input
+                            type="password"
+                            className="form-control bg-dark text-white py-0 px-3 "
+                            id="floatingName"
+                            name="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          ></input>
+                        </div>
+                        <div>
+                          <button
+                            class="btn btn-login px-5 btn-lg text-white"
+                            type="submit"
+                            onClick={handleSubmit}
+                          >
+                            Sign Up
+                          </button>
+                        </div>
+                      </form>
 
-                  <form>
-                    <div className="form-floating mb-3">
-                      <i class="icon fa-solid fa-envelope"></i>
-                      <input
-                        type="email"
-                        className="form-control bg-dark text-white"
-                        id="floatingName"
-                        placeholder="Email"
-                        onChange={(e) => setEmail(e.target.value)}
-                      ></input>
-                    <h4 className="errorhead text-danger"> {errors.map((error)=>(error))} </h4>
-
+                      <div class="form-outline form-white mb-2"></div>
+                      {/* <p class="small pb-lg-8">
+                        <a class="text-" href="#!">
+                          Forgot password?
+                        </a>
+                      </p> */}
                     </div>
-                    <div className="form-floating mb-3">
-                      <i class="icon fa-solid fa-unlock-keyhole"></i>
-                      <i class="icon fa-light fa-unlock-keyhole"></i>
-                      <input
-                        type="password"
-                        className="form-control bg-dark text-white"
-                        id="floatingName"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      ></input>
+
+                    <div>
+                      <p class="mb-5 code">
+                        Already have an account? {" "}
+                        <Link to="/login">
+                          <a href="" class="text fw-bold">
+                            Login
+                          </a>
+                        </Link>
+                      </p>
                     </div>
-                  </form>
-
-                  <button
-                    class="btn btn-login btn-lg px-5 text-white"
-                    type="submit"
-                    onClick={handleSubmit}
-                  >
-                    Sign Up
-                  </button>
-                </div>
-
-                <div>
-                  <p class="mb-0 code">
-                    Already have an account?{" "}
-                    <Link to="/login">
-                      <a href="#!" class="text fw-bold">
-                        Login
-                      </a>
-                    </Link>
-                  </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Signup;
